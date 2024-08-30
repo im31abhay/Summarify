d53f4c5bcbc5a6be76aaa314218513c2a08f419b
im31abhay <abhay31w@gmail.com>
2024-08-31 01:42:02 +0530

    Removing the cors error

---
+++ lib/chrome/chrome_api.dart
@@ -0,0 +1,30 @@
+// ignore_for_file: public_member_api_docs
+@JS('chrome')
+library chrome;
+
+// import 'dart:js_util';
+
+import 'package:js/js.dart';
+
+@JS('tabs.query')
+external Future<List<Tab>> query(final ParameterQueryTabs parameterQueryTabs);
+
+@JS()
+class Tab {
+  external String get url;
+
+  external factory Tab(final String url);
+}
+
+@JS()
+@anonymous
+class ParameterQueryTabs {
+  external factory ParameterQueryTabs({
+    final bool active,
+    final bool lastFocusedWindow,
+  });
+
+  external bool get active;
+
+  external bool get lastFocusedWindow;
+}
